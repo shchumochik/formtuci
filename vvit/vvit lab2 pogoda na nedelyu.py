ccfdb3ce084253a6db8fe6efc34d80ec848a0aa8
shchumochik <shchumochik@gmail.com>
2023-12-08 13:57:06 +0300

    1
    1

--- vvit/vvit lab2 pogoda na nedelyu.py
+++ vvit/vvit lab2 pogoda na nedelyu.py
@@ -1,12 +1,26 @@
 import requests
-s_city = 'Moscow, RU'
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q='+s_city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+city = "Moscow,RU"
+appid = "aeefc7317937b7b9d976af74831c4044"
+
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-params={'q': s_city, 'units': 'metric', 'lang': 'ru'})
-data = requests.get(url).json()
-print("Город:", s_city)
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Москва:", city)
 print("Погодные условия:", data['weather'][0]['description'])
 print("Температура:", data['main']['temp'])
 print("Минимальная температура:", data['main']['temp_min'])
 print("Максимальная температура", data['main']['temp_max'])
+print("Скорость ветра", data['wind']['speed'])
+print("Видимость", data['visibility'])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+    '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+    i['weather'][0]['description'], ">")
+print("Скорость ветра <", i['wind']['speed'], "> \r\nВидимость <", i['visibility'],">")
